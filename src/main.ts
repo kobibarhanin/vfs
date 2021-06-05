a2d4e298701ab55cd5e4b4988aaf646d851b8e4f
kobibarhanin <kobibarhanin@gmail.com>
2021-06-05 16:51:52 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,18 @@ import { DropboxStorage } from "./DropboxStorageService";
 
 (async ()=>{
 
+    // # log files
     let files;
 
-    let google_service = new GoogleStorage();
-    files = await google_service.files();
-    console.log(files)
-    // google_service.download("18EEAc3_k7Nz70eZZMcPuMDOq8FZPQ_h3", "items/test.jpg")
+    // let google_service = new GoogleStorage();
+    // files = await google_service.files();
+    // console.log(files)
 
     let dropbox_service = new DropboxStorage();
     files = await dropbox_service.files();
     console.log(files)
+
+    // # download files
+    // google_service.download("18EEAc3_k7Nz70eZZMcPuMDOq8FZPQ_h3", "items/test.jpg")
 
 })();
